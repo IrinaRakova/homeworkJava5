5a139de347baf32c7818e6a3b5e645bf3521fb3e
Ирина Ракова <irakova.education@gmail.com>
2023-10-23 21:34:11 +0300

    Changed2 commit

--- src/main/java/ru/netology/services/MoneyService.java
+++ src/main/java/ru/netology/services/MoneyService.java
@@ -4,8 +4,7 @@ public class MoneyService {
     public int calculate(int income, int expense, int threshold) {
         int count = 0;
         int money = 0;
-        for (int month = 1; month < 12; ) {
-            month++;
+        for (int month = 1; month < 12; month++) {
             money = money + income - expense;
             if (money >= threshold) {
                 money = (money - expense) / 3;
